fe1e6e646cc3c11539941a71028d19be356a93f4
allt2709 <renatotorrescalle@gmail.com>
2021-07-03 20:32:59 -0500

    Modify the Readme.md

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 module.exports = {
-  port: process.env.PORT || 3001,
+  port: process.env.PORT || 3000,
   mongoURI: process.env.NODE_ENV === 'test' ? process.env.MONGO_DB_URI_TEST : process.env.MONGO_DB_URI,
   secret: process.env.SECRET,
 };
